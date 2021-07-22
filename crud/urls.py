90f0bb1a2a3a02288badf656a9efc8851ca7ef84
Islom <nolove02@bk.ru>
2021-07-21 21:04:23 -0400

    updated crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.generic import DeleteView
 
-from crud.views import BookTemplateView, BookDetailView, BookFormView
+from crud.views import BookTemplateView, BookDetailView, BookFormView, update_book, delete_book
 
 urlpatterns = [
     path("book/", BookTemplateView.as_view(), name="index"),
     path("book/<int:pk>", BookDetailView.as_view(), name="book"),
     path("form/", BookFormView.as_view(), name="form"),
+    path("book/<int:pk>/update", update_book, name="update_book"),
+    path("book/<int:pk>/delete_book", delete_book, name="delete")
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView, DetailView, FormView, CreateView, DeleteView
 
 from .forms import BookModelForm
@@ -22,3 +22,28 @@ class BookFormView(CreateView):
 
     def form_valid(self, form):
         return super(BookFormView, self).form_valid(form)
+
+
+def update_book(request, pk):
+    if request.method == "POST":
+        book = get_object_or_404(BookModel, pk=pk)
+        form = BookModelForm(request.POST, files=request.FILES, instance=book)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect("/book/")
+    else:
+        book = get_object_or_404(BookModel, pk=pk)
+        form = BookModelForm(instance=book)
+
+    context = {
+        "form": form
+    }
+    return render(request, "forms.html", context)
+
+
+def delete_book(request, pk):
+    book = get_object_or_404(BookModel, pk=pk)
+    book.delete()
+    return redirect("/book/")
